75171a045f97a4888e235bb2dc20a91bb5d14254
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-09 23:12:08 +0630

    Define HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\In;
 
 class HomeController extends Controller
 {
-    //
+    public function home()
+    {
+        return inertia('Home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/', [HomeController::class,'home'])->name('dashboard');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
