00b4b720a171575fc3de1cc22d95efefb443bd91
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-10 00:15:30 +0630

    Modify Home.jsx and ChatLayout.jsx

---
+++ resources/js/Layouts/ChatLayout.jsx
@@ -0,0 +1,19 @@
+import { usePage } from "@inertiajs/react";
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+
+const ChatLayout = ({ children }) => {
+    const page = usePage();
+    const conversations = page.props.conversations;
+    const selectedConversation = page.props.selectedConversation;
+    console.log("conversations", conversations);
+    console.log("selectedConversation", selectedConversation);
+
+    return (
+        <AuthenticatedLayout>
+            ChatLayout
+            <div>{children}</div>
+        </AuthenticatedLayout>
+    );
+};
+
+export default ChatLayout;
