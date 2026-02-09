c68be0856ee527760120d397de899a967add4417
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-09 19:48:49 +0630

    Add 3 columns to groups table

--- database/migrations/2026_02_09_130515_create_groups_table.php
+++ database/migrations/2026_02_09_130515_create_groups_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->foreignId('owner_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
