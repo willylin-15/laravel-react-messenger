3d67bf8e095495fe1b660525dca592285a3ba42e
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-09 20:19:21 +0630

    Add users(), messages() and owner() functions

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -14,4 +14,19 @@ class Group extends Model
         'owner_id',
         'last_message_id',
     ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'group_users');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
