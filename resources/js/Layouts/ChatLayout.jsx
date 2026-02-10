9176b89186c321518120252140025bd00d1b6580
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-10 16:12:52 +0630

    Define channels

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,11 +1,29 @@
 import { usePage } from "@inertiajs/react";
+import { useEffect } from "react";
 
 const ChatLayout = ({ children }) => {
     const page = usePage();
     const conversations = page.props.conversations;
     const selectedConversation = page.props.selectedConversation;
+
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
+
+    useEffect(() => {
+        Echo.join("online")
+            .here((users) => {
+                console.log("here", users);
+            })
+            .joining((user) => {
+                console.log("joining", user);
+            })
+            .leaving((user) => {
+                console.log("leaving", user);
+            })
+            .error((error) => {
+                console.error("error", error);
+            });
+    }, []);
 
     return (
         <>
