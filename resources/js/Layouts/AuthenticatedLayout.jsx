1a7bb80009fe7ae8cb8bd68b56551e314ff6fab0
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-10 15:34:33 +0630

    Import echo.js

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -3,17 +3,13 @@ import Dropdown from "@/Components/Dropdown";
 import NavLink from "@/Components/NavLink";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
 import { Link, usePage } from "@inertiajs/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function AuthenticatedLayout({ header, children }) {
     const user = usePage().props.auth.user;
 
     const [showingNavigationDropdown, setShowingNavigationDropdown] =
         useState(false);
-
-    useEffect(() => {
-        console.log("AuthenticatedLayout mounted");
-    }, []);
 
     return (
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
