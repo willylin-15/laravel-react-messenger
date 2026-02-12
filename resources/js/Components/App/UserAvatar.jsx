0a597d58c0426b171e0812977b1921682d8c4cb3
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-13 00:42:58 +0630

    Make avatar-online work

--- resources/js/Components/App/UserAvatar.jsx
+++ resources/js/Components/App/UserAvatar.jsx
@@ -1,20 +1,20 @@
 const UserAvatar = ({ user, online = null, profile = false }) => {
     let onlineClass =
-        online === true ? "online" : online === false ? "offline" : "";
+        online === true ? "avatar-online" : online === false ? "avatar-offline" : "";
 
     const sizeClass = profile ? "w-40" : "w-8";
 
     return (
         <>
             {user.avatar_url && (
-                <div className={`chat-image avatar avatar-${onlineClass}`}>
+                <div className={`chat-image avatar ${onlineClass}`}>
                     <div className={`rounded-full ${sizeClass}`}>
                         <img src={user.avatar_url} />
                     </div>
                 </div>
             )}
             {!user.avatar_url && (
-                <div className={`chat-image avatar avatar-placeholder avatar-${onlineClass}`}>
+                <div className={`chat-image avatar avatar-placeholder ${onlineClass}`}>
                     <div className={`bg-gray-400 text-gray-800 rounded-full ${sizeClass}`}>
                         <span className=" text-xl">
                             {user.name.substring(0, 1)}
