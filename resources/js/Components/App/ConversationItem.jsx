8192f59670906fcc8d17a3f733592f3e4a42196a
Wai Yan Lin <willywaiyanlin@gmail.com>
2026-02-16 00:06:19 +0630

    Modify displaying date format in ConvItem

--- resources/js/Components/App/ConversationItem.jsx
+++ resources/js/Components/App/ConversationItem.jsx
@@ -2,6 +2,7 @@ import { Link, usePage } from "@inertiajs/react";
 import UserAvatar from "./UserAvatar";
 import GroupAvatar from "./GroupAvatar";
 import UserOptionsDropdown from "./UserOptionsDropdown";
+import { formatMessageDateShort } from "@/helpers";
 
 const ConversationItem = ({
     conversation,
@@ -60,7 +61,7 @@ const ConversationItem = ({
                 </div>
                 {conversation.last_message && (
                     <p className=" text-xs text-nowrap overflow-hidden text-ellipsis">
-                        {conversation.last_message}
+                        {formatMessageDateShort(conversation.last_message)}
                     </p>
                 )}
             </div>
